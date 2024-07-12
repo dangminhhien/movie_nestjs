2f8837a1c58a866bdc08afac86b4db38be3a1a50
dangminhhien <dangminhhien112@gmail.com>
2024-07-12 11:17:02 +0700

    first commit

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param, Render } from '@nestjs/common';
 import { CourseService } from './course.service';
 
-@Controller('couese')
+@Controller('course')
 export class CourseController {
     constructor(private readonly courseService: CourseService) {}
 
